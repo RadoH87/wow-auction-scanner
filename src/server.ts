75ec511f403e71d48ef34b44e44aa602aec7d409
Rado <radekh8787@gmail.com>
2024-09-14 04:40:46 +0100

    Refactor: Split routes into separate auction and item routers, updated 'server.ts', and removed 'api.ts'

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,27 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
-import { getAuctionData } from "./controllers/auctionController";
-import { getAuctionsByRealm } from "./controllers/realmController";
 import {
   loadRaidBotsData,
   loadEquipableItemsData,
 } from "./utils/loadJsonFiles";
-import { generateItems } from "./controllers/itemController";
+import { auctionRouter } from "./routes/auctionRouter";
+import { itemRouter } from "./routes/itemRouter";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json()); // Middleware do parsowania JSON
+// Parse JSON Middleware
+app.use(express.json());
 
 (async () => {
   // Load necessary data before starting the server
   await loadRaidBotsData();
   await loadEquipableItemsData();
 
-  app.get("/api/auctions", getAuctionData);
-  app.get("/api/auctions/:realmSlug", getAuctionsByRealm);
-
-  // Endpoint for generate items list
-  app.post("/api/generate-items", generateItems);
+  app.use("/api/auctions", auctionRouter);
+  app.use("/api", itemRouter);
 
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
